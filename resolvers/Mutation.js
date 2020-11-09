d5553241f5830e8eb5a812029b5a0cc3b0dc4345
Ivan Pinheiro <ivaneletronica00@gmail.com>
2020-11-08 21:33:52 -0300

    Atualizando as Mutations

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -2,17 +2,45 @@ const { usuarios, proximoId } =
 require('../data/db')
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade}) {
+    // nome, email, idade
+    novoUsuario(_, args ) {
+        const emailExistente = usuarios
+            .some( u => u.email === args.email )
+        
+        if(emailExistente) {
+            throw new Error('Email cadastrado')
+        }
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO'
         }
 
         usuarios.push(novo)
         return novo
+    },
+    excluirUsuario(_, { id }) {
+        const i = usuarios
+            .findIndex(u => u.id === id)
+        if(i <0) return null
+        const excluidos = 
+            usuarios.splice(i, 1)
+        return  excluidos ? 
+            excluidos[0] : null
+    },
+    alterarUsuario(_,args){
+        const i = usuarios
+            .findIndex(u => u.id === args.id)
+        if(i < 0) return null
+
+        usuarios[i]
+        // const usuario = {
+        //     ...usuarios[i],
+        //     ...args
+        // }
+
+        usuarios.splice(i, 1, usuario)
+        return usuario
     }
 }
